20ec4eaca406d70a444367a654a94484cfcd6c4e
“tsukasatakahashi21” <tukaburu13@gmail.com>
2024-10-07 21:24:22 +0900

    管理者専用のログイン機能

---
+++ docker/haru/src/app/Http/Controllers/AuthController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Admin;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    // 新規登録フォームを表示
+    public function showRegisterForm()
+    {
+        return view('admin.register');
+    }
+
+    // 登録処理
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:admins',
+            'password' => 'required|string|min:6|max:255|regex:/[a-z]/|regex:/[A-Z]/|regex:/[0-9]/'
+        ], [
+            'name.required' => 'Nameの入力がありません',
+            'email.required' => 'Emailの入力がありません',
+            'email.unique' => 'このEmailはすでに使用されています',
+            'password.required' => 'Passwordの入力がありません',
+            'password.min' => 'Passwordは6文字以上である必要があります',
+            'password.regex' => 'Passwordには小文字、大文字、数字を含める必要があります',
+        ]);
+
+        Admin::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+        ]);
+
+        return redirect()->route('admin.login');
+    }
+
+    // ログインフォームを表示
+    public function showLoginForm()
+    {
+        return view('admin.login');
+    }
+
+    // ログイン処理
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email|max:255',
+            'password' => 'required|string|min:6|max:255|regex:/[a-z]/|regex:/[A-Z]/|regex:/[0-9]/'
+        ], [
+            'email.required' => 'Emailの入力がありません',
+            'password.required' => 'Passwordの入力がありません',
+            'password.min' => 'Passwordは6文字以上である必要があります',
+            'password.regex' => 'Passwordには小文字、大文字、数字を含める必要があります',
+        ]);
+
+        $loginData = $request->only('email', 'password');
+
+        if (Auth('admin')->attempt($loginData)) {
+            return redirect()->intended('/admin/dashboard');
+        }
+        return back()->withErrors([
+            'email' => 'ログイン情報が正しくありません',
+        ]);
+    }
+
+    // ログアウト処理
+    public function logout()
+    {
+        Auth::guard('admin')->logout();
+        return redirect()->route('admin.login');
+    }
+}
---
+++ docker/haru/src/database/migrations/2024_10_06_051425_create_admins_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->uuid('id', 36)->primary();
+            $table->string('name', 255);
+            $table->string('email', 255)->unique();
+            $table->string('password', 255);
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('admins');
+    }
+};
