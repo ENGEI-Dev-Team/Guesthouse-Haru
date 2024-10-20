045904bd2d7a4d969477b8e0642001f1c1fd1c88
“tsukasatakahashi21” <tukaburu13@gmail.com>
2024-10-20 20:19:48 +0900

    モーダルの表示

--- docker/haru/src/resources/views/user/reservation.blade.php
+++ docker/haru/src/resources/views/user/reservation.blade.php
@@ -25,38 +25,105 @@
 
 <section class="booking-detail">
   <div class="inner">
-    <h2 class="section-title">Pricing & Booking Information</h2>
+    <h2 class="section-title">Pricing & House Information</h2>
 
     <div class="detail-group">
       <div class="detail-head">
-        <p>Basic price</p>
+        <p>Basic price (7 guests maximum)</p>
       </div>
       <div class="detail-text">
-        <p>¥25,000 per room (excluding tax)</p>
+        <p>¥25,000 per room </p>
       </div>
     </div>
 
     <div class="detail-group">
       <div class="detail-head">
-        <p>Additional charges per person</p>
+        <p>Additional charge from 3 guests</p>
       </div>
       <div class="detail-text">
-        <p>Additional charge from 3 guests: ¥8,000 per person per night (excluding tax)</p>
-        <p>Elementary school students: ¥4,000 per night (excluding tax)</p>
-        <p>Children under 6 years old (pre-school age): Free</p>
+        <p> ¥8,000 per person (Adult) </p>
+        <p>¥4,000 per night (Elementary school students)</p>
+        <p>Free (Under 6 years old)</p>
       </div>
     </div>
 
     <div class="detail-group">
       <div class="detail-head">
-        <p>Check-in / out</p>
+        <p>Check-in</p>
       </div>
       <div class="detail-text">
-        <p>15:00 ~ / ~ 10:00</p>
+        <p>15:00～18:00</p>
+      </div>
+    </div>
+
+    <div class="detail-group">
+      <div class="detail-head">
+        <p>Check-out</p>
+      </div>
+      <div class="detail-text">
+        <p>~ 10:00</p>
+      </div>
+    </div>
+
+    <div class="detail-group">
+      <div class="detail-head">
+        <p>Meals</p>
+      </div>
+      <div class="detail-text">
+        <p>¥800 for Breakfast at the guesthouse. If you would like to have breakfast, please let us know when making your reservation.</p>
+      </div>
+    </div>
+
+    <div class="detail-group">
+      <div class="detail-head">
+        <p>Adrress</p>
+      </div>
+      <div class="detail-text">
+        <p>2488 Karato, Tonosho-cho, Shodoshima District, Kagawa, Japan</p>
       </div>
     </div>
   </div>
 </section>
+
+
+<div id="overlay" class="overlay"></div>
+<div id="infoModal" class="modal">
+  <div class="modal-content">
+    <button id="closeModal" class="close-button" aria-label="Close modal">&times;</button>
+    <h2 class="modal-title">House Rules and Important Information</h2>
+
+    <p class="modal-text"><strong>1.</strong> Please minimize noise during nighttime hours from 9:00 PM.</p>
+
+    <p class="modal-text"><strong>2.</strong> The entire facility is non-smoking. Please use designated areas for smoking, including heated tobacco products.</p>
+
+    <p class="modal-text"><strong>3.</strong> During the Setouchi Triennale, traffic may be heavy, with many elderly drivers on the island. Please drive carefully and observe traffic manners.</p>
+
+    <p class="modal-text"><strong>4.</strong> There are no convenience stores and ATMs are limited to Japan Post Bank and JA Bank. Most transactions are cash-based, so please ensure you have sufficient cash and necessary items before your visit.</p>
+
+    <p class="modal-text"><strong>5.</strong> Access to the house is restricted to registered guests only; non-guests are not allowed to enter.</p>
+
+    <p class="modal-text"><strong>6.</strong> Guests will be held responsible for any damage or destruction of the facility caused by negligence. Lost keys will also incur a compensation fee.</p>
+
+    <p class="modal-text"><strong>7.</strong> In the event of a medical emergency, there are no hospitals on the island. You will need to arrange and pay for a sea taxi to go to hospital in Uno or Takamatsu.</p>
+
+    <p class="modal-text"><strong>Important:</strong> 
+    After your reservation is confirmed, we will send you a Google Form to confirm details. We appreciate your cooperation in providing responses to ensure a more comfortable stay.</p>
+
+    <div class="modal-confirm">
+      <label>
+        <input type="checkbox" id="confirm-check" required>
+        I have read and understood the rules.
+      </label>
+    </div>
+
+    <div class="modal-buttons">
+      <button id="airbnbButton" disabled>Book from Airbnb</button>
+      <button id="vacationStayButton" disabled>Book from VacationSTAY</button>
+    </div>
+  </div>
+</div>
+
+
 @endsection
 
 @section('scripts')
@@ -72,11 +139,60 @@
       initialView: 'dayGridMonth',
       events: events,
       eventClick: function(info) {
-        alert('Available date clicked: ' + info.event.start.toISOString().slice(0, 10));
+        if (info.event.extendedProps.clickable) {
+          showModal();
+        }
       },
+      eventContent: function(info) {
+        return {
+          html: '<span>' + info.event.title + '</span>'
+        };
+      }
     });
 
     calendar.render();
+
+    function showModal() {
+      const closeButton = document.getElementById('closeModal');
+      const modal = document.getElementById('infoModal');
+      const confirmCheck = document.getElementById('confirm-check');
+      const airbnbButton = document.getElementById('airbnbButton');
+      const vacationStayButton = document.getElementById('vacationStayButton');
+
+      overlay.style.display = 'flex'; 
+      modal.style.display = 'flex';
+
+      closeButton.addEventListener('click',closeModal); 
+      overlay.addEventListener('click', closeModal);
+
+
+      confirmCheck.addEventListener('change', function() {
+        if (confirmCheck.checked) {
+          airbnbButton.disabled = false;
+          vacationStayButton.disabled = false;
+        } else {
+          airbnbButton.disabled = true;
+          vacationStayButton.disabled = true;
+        }
+      });
+
+      airbnbButton.addEventListener('click', function() {
+        window.location.href = 'https://www.airbnb.com/h/shimayado-haru';
+      });
+
+      vacationStayButton.addEventListener('click', function() {
+        window.location.href = 'https://vacation-stay.jp/listings/266521';
+      });
+
+      function closeModal() {
+        confirmCheck.checked = false;
+        airbnbButton.disabled = true;
+        vacationStayButton.disabled = true;
+
+        modal.style.display = 'none';
+        overlay.style.display = 'none'; 
+      }
+    }
   });
 </script>
 @endsection
