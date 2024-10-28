3bebcabe4060e727ac6c032d4cbee12781ea867a
“tsukasatakahashi21” <tukaburu13@gmail.com>
2024-10-28 23:32:56 +0900

    管理者用のカレンダー機能のリファクタリング

--- docker/haru/src/app/Http/Controllers/AdminCalendarController.php
+++ docker/haru/src/app/Http/Controllers/AdminCalendarController.php
@@ -2,9 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use Google_Client;
-use Google_Service_Calendar;
-use Illuminate\Http\Request;
 use Sabre\VObject\Reader;
 
 class AdminCalendarController extends Controller
@@ -13,7 +10,6 @@ class AdminCalendarController extends Controller
     {
         // Airbnbカレンダーから予約情報を取得
         $airbnbData = $this->fetchAirbnbCalendar();
-
         return $this->convertToAvailableEvents($airbnbData);
     }
 
@@ -24,26 +20,33 @@ class AdminCalendarController extends Controller
         $icalContent = file_get_contents($icalUrl);
         $vcalendar = Reader::read($icalContent);
 
-        return array_filter(array_map(function ($event) {
-            $summary = (string)$event->SUMMARY;
-            // "Reserved"のSUMMARYを持つイベントだけを取得
-            if ($summary === "Reserved") {
-                $description = (string)$event->DESCRIPTION;
-                // URLを抽出
-                preg_match('/Reservation URL: (https[^"]+)/', $description, $matches);
-                $url = $matches[1] ?? null;
+        return array_filter(array_map([$this, 'extractEventData'], $vcalendar->select('VEVENT')));
+    }
 
-                return [
-                    'start' => $this->formatDateTime($event->DTSTART),
-                    'end' => $this->formatDateTime($event->DTEND),
-                    'url' => $url,
-                    'description' => '予約あり',
-                ];
-            }
-            return null; //
-        }, $vcalendar->select('VEVENT')), function ($event) {
-            return !is_null($event); 
-        });
+    // イベントデータを抽出
+    private function extractEventData($event)
+    {
+        $summary = (string)$event->SUMMARY;
+
+        // "Reserved"のSUMMARYを持つイベントだけを取得
+        if ($summary === "Reserved") {
+            $url = $this->extractReservationUrl((string)$event->DESCRIPTION);
+
+            return [
+                'start' => $this->formatDateTime($event->DTSTART),
+                'end' => $this->formatDateTime($event->DTEND),
+                'url' => $url,
+                'description' => '予約あり',
+            ];
+        }
+        return null;
+    }
+
+    // URLを抽出
+    private function extractReservationUrl($description)
+    {
+        preg_match('/Reservation URL: (https[^"]+)/', $description, $matches);
+        return $matches[1] ?? null;
     }
 
     // 日付時間をフォーマット
